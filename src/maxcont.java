89f65a25f8e32a1898fc3de79085964ab052a3fb
ACER <havishnadar@gmail.com>
2020-12-14 15:14:19 +0530

    Hello

--- src/maxcont.java
+++ src/maxcont.java
@@ -2,26 +2,28 @@ import java.util.Scanner;
 
 public class maxcont {
     public static void main(String args[]){
-        int s1;
+        int s1,key;
         Scanner sd=new Scanner(System.in);
         System.out.println("Enter the size of the array");
         s1=sd.nextInt();
+        System.out.println("Enter the key value");
+        key=sd.nextInt();
+        System.out.println("Enter the array elements");
         int[] array=new int[s1];
         for (int i=0;i<s1;i++){
             array[i]=sd.nextInt();
         }
         int sum1=0;
         for (int i=0;i<s1;i++){
-            if (array[i]>0){
-                sum1=sum1+array[i];
+            if(array[i]==key){
+                System.out.print("{"+array[i]+"}");
+                continue;
             }
-            else {
-                if (array[i+1]>0){
-                    sum1=sum1+array[i];
+            for (int j=i+1;j<s1;j++){
+                if(array[i]+array[j]==key){
+                    System.out.print("{"+array[i]+" "+array[j]+"}");
                 }
-                else {
 
-                }
             }
         }
     }
